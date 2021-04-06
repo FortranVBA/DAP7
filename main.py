65079c19a3aa6deafe14e9d41dea984896655936
FortranVBA <mpyfabrice@gmail.com>
2021-04-06 11:41:48 +0200

    Création fichiers vierges

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Project OC DAP 7 main file."""
+
+def main():
+    """Program entry point."""
+    #app = Application()
+    #app.run()
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
